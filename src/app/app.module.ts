1f6fd901868986fff5311a1e3a0772f00b6ea082
Riadhchibani <riadhchibani144@gmail.com>
2022-09-18 12:20:27 +0200

    added search page and new design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,19 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JobSearchComponent } from './job-search/job-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    JobSearchComponent
   ],
   imports: [
     BrowserModule,
@@ -23,9 +27,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatIconModule,
     MatGridListModule,
     MatCardModule,
+    MatButtonModule,
+    MatExpansionModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
+      { path: 'jobDetails', component: JobSearchComponent },
     ]),
     BrowserAnimationsModule
   ],
---
+++ src/app/job-search/job-search.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-job-search',
+  templateUrl: './job-search.component.html',
+  styleUrls: ['./job-search.component.css']
+})
+export class JobSearchComponent implements OnInit {
+
+  panelOpenState = false;
+  constructor() { }
+  accept(){}
+  ngOnInit(): void {
+  }
+
+}
